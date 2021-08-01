e7dc532f64a2f5ce91b131d9180361112081ed5f
RobARC <rrondonc@gmail.com>
2021-08-01 13:09:56 -0500

    update file fix if function recieved a empty list

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -14,7 +14,7 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *newnode = NULL, *aux = NULL;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (NULL);
 
 	newnode = malloc(sizeof(struct listint_s)); /*creating newnode*/
