dc3df304413c4de3f296da60d0d5fe3f23d50db0
Roberth <rrondoc@gmail.com>
2021-08-25 12:47:47 -0500

    update sandpiles.h betty styles

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,12 +1,14 @@
-#ifndef SANDPILES__H
-#define SANDPLIES__H
+#ifndef SANDPILES_H
+#define SANDPLIES_H
 
 #include <stdlib.h>
 #include <stdio.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
 void tupple(int sandpiles[3][3], int grid2[3][3]);
+
 int stable(int grid[3][3]);
 
 
-#endif /* SANDPLIES__H */
+#endif /* SANDPLIES_H*/
