40d73a1c1350a15dcd5d254a54c45128f0504a2d
RobARC <rrondoc@gmail.com>
2022-06-24 11:26:07 -0500

    Update file 0-making_change.py

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -13,19 +13,11 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    if len(coins) <= 0:
+    coins.sort(reverse=True)
+    count = 0
+    for coin in coins:
+        count += total // coin
+        total = total % coin
+    if total:
         return -1
-
-    change = sorted(coins)
-    change = change[::-1]
-
-    coins = 0
-    i = total
-    for c in change:
-        while (i - c >= 0):
-            i = i - c
-            coins = coins + 1
-
-    if i != 0 and i - change[-1] < 0:
-        return -1
-    return coins
+    return count
