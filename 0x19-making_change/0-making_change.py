99c18607e23d072a8f8815a9cc51344e6579e140
RobARC <rrondoc@gmail.com>
2022-06-24 11:12:56 -0500

    Update file 0-making_change.py

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -9,16 +9,23 @@ def makeChange(coins, total):
     """ Given a pile of coins of different values, determine the fewest
     number of coins needed to meet a given amount total.
     Return: fewest number of coins needed to meet total """
-    if total <= 0:
-        return
 
-    change = 0
-    coins.sort(reverse=True)
-    for coin in coins:
-        temp_change = int(total / coin)
-        total -= (temp_change * coin)
-        change += temp_change
-        if total == 0:
-            return change
-    if total != 0:
+    if total <= 0:
+        return 0
+
+    if len(coins) <= 0:
         return -1
+
+    change = sorted(coins)
+    change = change[::-1]
+
+    coins = 0
+    i = total
+    for c in change:
+        while (i - c >= 0):
+            i = i - c
+            coins = coins + 1
+
+    if i != 0 and i - change[-1] < 0:
+        return -1
+    return coins
